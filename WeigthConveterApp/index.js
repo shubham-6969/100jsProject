58f7b2d9c0acda9fb4d7b657ccab59b50f4bd158
Shubham Patwal <shubhampatwal695@gmail.com>
2025-12-10 20:20:12 +0530

    Weigth Converter project added

---
+++ WeigthConveterApp/index.js
@@ -0,0 +1,26 @@
+const inputEl = document.getElementById('input');
+const resultEl = document.getElementById('result');
+const errorEl = document.getElementById('error');
+let errorTime;
+let resultTime;
+
+function weigthConverter() {
+    // validation
+    if (inputEl.value <= 0 || isNaN(inputEl.value)) {
+        errorEl.innerText = "Please enter a valid number!"
+        clearTimeout(errorTime);
+        errorTime = setTimeout(() => {
+            errorEl.innerText = "";
+            inputEl.value = '';
+        }, 2000)
+    } else {
+        resultEl.innerText = (+inputEl.value / 2.2).toFixed(2);
+        clearTimeout(resultTime);
+        resultTime = setTimeout(() => {
+            resultEl.innerText = "";
+            inputEl.value = "";
+        }, 10000);
+    }
+}
+
+inputEl.addEventListener('input', weigthConverter)
